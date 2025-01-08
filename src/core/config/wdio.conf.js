ff9e26476bed78366e874c2cd585ae3a47d4e8e8
Mag József <mag.jozsef@dmjvph.net>
2025-01-08 10:20:41 +0100

    Updated  project

--- src/core/config/wdio.conf.js
+++ src/core/config/wdio.conf.js
@@ -21,7 +21,7 @@ export const config = {
     // of the config file unless it's absolute.
     //
     specs: [
-        './../tests/**.js'
+        './../../tests/**.js'
     ],
     // Patterns to exclude.
     exclude: [
